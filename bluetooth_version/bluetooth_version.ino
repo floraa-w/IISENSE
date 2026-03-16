9f4a6969649ad6c77e0c6b41adf9e34237f952b6
San <basnetsan25@gmail.com>
2026-03-16 17:27:13 -0400

    :/

--- bluetooth_version/bluetooth_version.ino
+++ bluetooth_version/bluetooth_version.ino
@@ -20,7 +20,6 @@
  */
 
 #include <ArduinoJson.h>
-
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
@@ -29,9 +28,9 @@
 // ── BLE settings ─────────────────────────────────────────────────
 const char* BT_DEVICE_NAME = "Sense-ESP32";
 
-#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
-#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
-#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+#define SERVICE_UUID           "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
+#define CHARACTERISTIC_UUID_RX "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
+#define CHARACTERISTIC_UUID_TX "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
 
 BLECharacteristic* pTxCharacteristic = nullptr;
 bool deviceConnected = false;
